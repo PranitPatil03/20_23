ab8ac8e3bb262d9836b30f239f098dfffbe212f8
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-04 01:42:01 +0530

    JavaScript Array Methods

---
+++ Revision/Javascript/Arrays/script.js
@@ -0,0 +1,55 @@
+const objects=[
+    {
+        name:"test1",
+        age:20,
+        gender:"male",
+        sal:'100000'
+    },
+    {
+        name:"test2",
+        age:18,
+        gender:"female",
+        sal:'20000'
+    },
+    {
+        name:"test3",
+        age:26,
+        gender:"male",
+        sal:'200000'
+    },
+    {
+        name:"test4",
+        age:23,
+        gender:"female",
+        sal:'80000'
+    }
+]
+
+// Array Methods 
+
+// Method 1
+
+// for (var i=0; i<object.length; i++){
+//     console.log(object[i]);
+// }
+
+// Method 2
+
+// const object=[];
+
+// for (var i=0; i<objects.length; i++){
+//     object.push(objects[i]);
+// }
+
+// console.log(object);
+
+// Method 3
+
+// objects.forEach(element => {
+//     object.push(element)
+// });
+
+// console.log(object);
+
+// objects.map(element =>console.log(element));
+
