2d2748be874bf13318e4033513141b9374a198ef
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-16 01:47:57 +0530

    Tic-Tac-Toe UI Completed

--- Revision/React/tic_tac_toe/src/App.js
+++ Revision/React/tic_tac_toe/src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import Board from './compoenents/Board';
+import { useState } from 'react';
 
 function App() {
+
+  const [ board, setBoard] = useState(Array(9).fill(null));
+
+  const [ xPlaying, setXPlaying] = useState(true);
+
+  const handleBoxClick = boxIdx =>{
+    const updatedBoard = board.map((value,idx)=>{
+      if(idx === boxIdx){
+        return xPlaying ===true ? "X" : "o";
+      }
+      else{
+        return value;
+      }
+    })
+
+    setBoard(updatedBoard)
+
+    setXPlaying(!xPlaying)
+  }
+
   return (
-    <div> 
-      Tic Tac Toe Game
+    <div className='App'> 
+      <Board board={board} onClick={handleBoxClick}/>
     </div>
   );
 }
