c61d8a9997e9e02c3dcae7b35dd3bf80d02283aa
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2023-07-15 23:37:32 +0530

    React Projects Setup Completed

---
+++ Revision/React/search_bar/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Search Bar Project 
+    </div>
+  );
+}
+
+export default App;
---
+++ Revision/React/tic_tac_toe/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div> 
+      Tic Tac Toe Game
+    </div>
+  );
+}
+
+export default App;
